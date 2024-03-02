4d6efdcd46d998fe41b23a0d74c16a66549fdd5a
kazu <48248903+kazudotdev@users.noreply.github.com>
2024-03-02 16:01:59 +0900

    backend: add new environment variables

--- apps/backend/src/env.ts
+++ apps/backend/src/env.ts
@@ -2,8 +2,11 @@ import { type Context } from "hono";
 import { env as _env } from "hono/adapter";
 
 type Env = Partial<{
+  PASSKEYS_SECRET_KEY: string;
   PASSKEYS_URL: string;
   PASSKEYS_ADMIN_URL: string;
+  LIBSQL_URL: string;
+  LIBSQL_ADMIN_URL: string;
 }>;
 
 export const env = (c: Context) => _env<Env>(c);
