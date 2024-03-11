749fe5cd456d957c141e999115e20f04d6d0cce9
kazu <48248903+kazudotdev@users.noreply.github.com>
2024-03-11 23:32:32 +0900

    db: rename directory

--- packages/admin-db/src/client.ts
+++ packages/admin-db/src/client.ts
@@ -2,6 +2,7 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient as _createClient } from "@libsql/client";
 import * as schema from "./schema";
 
-export const createClient = (url: string, { logger }: { logger?: boolean }) => {
+export const createClient = (url: string, options?: { logger?: boolean }) => {
+  const logger = options?.logger;
   return drizzle(_createClient({ url }), { schema, logger });
 };
